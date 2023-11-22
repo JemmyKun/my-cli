1a943b2517c149ec8e45a4300e9333af0a9df040
JemmyLi <403767069@qq.com>
2023-11-22 16:40:26 +0800

    Add files via upload

---
+++ bin/templates.js
@@ -0,0 +1,11 @@
+/** 暴露模版代码 */
+module.exports = [
+  {
+    name: "webpack5-react-ts",
+    value: "https://github.com:JemmyKun/electron-study",
+  },
+  {
+    name: "react18-vite2-ts",
+    value: "https://github.com:JemmyKun/frontend-study",
+  },
+];
